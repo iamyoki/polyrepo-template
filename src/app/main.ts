1018c1c897161d6ba00583e9e15587dcc8eb4c0f
Yoki <iamyoki@users.noreply.github.com>
2025-11-28 23:57:59 +0800

    chore: add some code

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,2 +1,7 @@
+import { a } from "../lib/a.ts";
+import { b } from "../lib/b.ts";
+import { c } from "../lib/c.ts";
+
 // I'm a running app, could be a node server.
+console.log(a, b, c);
 console.log(`I'm a running app, could be a node server....`);
